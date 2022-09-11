3d450111a8d45123bbcf3e0f9ebbc59dc048147a
I-Make-Gamez <ebelcher143@gmail.com>
2022-09-11 17:11:44 -0400

    j

--- doge-clicker/beta/java.js
+++ doge-clicker/beta/java.js
@@ -31,23 +31,27 @@ var exp = 0;
 var expCap = 500;
 var doExpProgression = true;
 localStorage.setItem('dep', doExpProgression)
+var vw = page.innerWidth
+var vh = page.innerHeight
+var unlock2 = false;
 
 
 //Version Start
-var version = 'vb0.6'
+var version = 'vb0.7'
 var page = window
+$(".av").hide()
 
-page.onload = function(){
+page.onload = function () {
     ver.innerHTML = `Version: ${version}`
     let doco = Math.floor(localStorage.getItem('tdc'))
     dc.innerHTML = `DogeCoin: ${doco}`
 }
 
-ver.addEventListener('click', function(){
+ver.addEventListener('click', function () {
     cl.style.zIndex = 1
     pu.style.opacity = 1;
 })
-ex.addEventListener('click', function(){
+ex.addEventListener('click', function () {
     cl.style.zIndex = -2
     pu.style.opacity = 1;
 })
@@ -55,39 +59,54 @@ ex.addEventListener('click', function(){
 
 //EL Start
 
-doge.addEventListener('click', function(){
+doge.addEventListener('click', function () {
     doExpProgression = localStorage.getItem('dep')
     doco += clipo;
     localStorage.setItem('tdc', doco)
     dc.innerHTML = `DogeCoin: ${doco}`
-    anim()
-    switch(doExpProgression){
+    if (plvl >= 5) {
+        if (unlock2 === false) {
+            $(".cos2").css("filter", "brightness(100%)");
+            popup.create("msg", "You've just unlocked a new avatar!<br>Go check it out in the avatar selection menu!", "Avatar Unlock!");
+            $(".cosTit").text("Toony Doge")
+            unlock2 = true
+        }
+    }
+    switch (doExpProgression) {
         case 'true':
             expCalc();
             break;
-        default:break;
+        default: break;
     }
+    anim()
 });
 
-cd.addEventListener('click', function(){
+$(".avs").on('click', function () {
+    $(".doge").fadeOut("slow")
+    $(".av").fadeIn("slow")
+})
+$(".cl").click(function () {
+    $(".av").fadeOut("slow")
+    $(".doge").fadeIn("slow")
+})
+
+cd.addEventListener('click', function () {
     popup.create("msg", "Gives you 25% of click power for 30 seconds.<br>Cooldown: 1 minute", "Alert!");
 })
 
 let codes = [
-    1807, 
+    1807,
 ];
 
-u1.addEventListener('click', function(){
-    if(doco >= dfPrice){
+u1.addEventListener('click', function () {
+    if (doco >= dfPrice) {
         clipo += 1;
         clicpo.innerHTML = `ClickPower: ${clipo}`
         doco -= dfPrice;
-        var tempval = Math.round(1.5 * dfPrice)
+        var tempval = Math.round(0.1 * dfPrice)
         dfPrice += Math.trunc(tempval, 0)
         dfCost.innerHTML = `Cost: ${dfPrice}DC`;
         dc.innerHTML = `DogeCoin: ${doco}`
-        
-        
     }
 })
 
@@ -95,19 +114,29 @@ u1.addEventListener('click', function(){
 
 //Functtions Start
 
-function anim(){
-    doge.style.content = "url(./assets/DOGE2.png)"
-    setTimeout(function () {
-        doge.style.content = "url(./assets/DOGE.png)"
-    }, 200)
+function anim() {
+    switch (selCos) {
+        case 1:
+            $(".doge").attr({
+                "src": "assets/DOGE2.png"
+            })
+            setTimeout(function () {
+                $(".doge").attr({
+                    "src": "assets/DOGE.png"
+                })
+            }, 200)
+            break;
+        default: break;
+    }
+
 }
 
 var plvl = 1;
 
-function expCalc(){
-    exp += 10*clipo+0;
+function expCalc() {
+    exp += 1 + (Math.trunc(clipo - 1 / 10));
     ec.innerHTML = `Exp: ${exp} / ${expCap}`
-    if(exp >= expCap){
+    if (exp >= expCap) {
         plvl += 1;
         expCap += 250;
         exp = 0;
@@ -142,7 +171,12 @@ document.onkeyup = function (e) {
                     //localStorage.setItem('clickpower', clipo)
                     break;
                 case '3':
-                    switch(coAns2[2]){
+                    var total = Math.floor(coAns2[2]);
+                    plvl += total;
+                    lvl.innerHTML = `Level: ${plvl}`
+                //localStorage.setItem('clickpower', clipo)
+                case 'exp':
+                    switch (coAns2[2]) {
                         case 'true':
                             var doExpProgression = true;
                             localStorage.setItem('dep', doExpProgression)
@@ -159,9 +193,157 @@ document.onkeyup = function (e) {
     }
 };
 
-document.onkeydown = function(e){
+$(".left").hover(function () { //hover
+    $(this).animate({ "left": "-=10px" }, "250");
+
+},
+    function () { //out
+        $(this).animate({ "left": "+=10px" }, "250");
+    }
+);
+
+$(".select").hover(function () { //hover
+    $(this).animate({
+        "font-size": "30px"
+    }, 250);
+
+},
+    function () { //out
+        $(this).animate({
+            "font-size": "25px"
+        }, 250);
+    }
+);
+
+$(".right").hover(function () { //hover
+    $(this).animate({ "left": "+=15px" }, "250");
+
+},
+    function () { //out
+        $(this).animate({ "left": "-=15px" }, "250");
+    }
+);
+
+var cosList = [1, 2]
+var cos = 1
+var cosMax = cosList.length;
+var selCos = 1
+$(".cos2").hide()
+
+
+
+$(".right").click(function () {
+    cos += 1
+    if (cos > cosMax) {
+        cos = 1;
+    }
+    switch (cos) {
+        case 1:
+            $(".cos1").fadeIn(250)
+            $(".cos2").fadeOut(250)
+            if (selCos === cos) {
+                $(".cosTit").text("Doge (Equipped)")
+            } else {
+                $(".cosTit").text("Doge")
+            }
+            break;
+        case 2:
+            $(".cos2").fadeIn(250)
+            $(".cos1").fadeOut(250)
+            if (selCos === cos) {
+                $(".cosTit").text("Toony Doge (Equipped)")
+            } else {
+                if (plvl <= 4) {
+                    $(".cosTit").text("Toony Doge (Locked)")
+                } else {
+                    $(".cosTit").text("Toony Doge")
+                }
+            }
+            break;
+        default: break;
+    }
+})
+$(".left").click(function () {
+    cos -= 1
+    if (cos < 1) {
+        cos = cosMax;
+    }
+    switch (cos) {
+        case 1:
+            $(".cos1").fadeIn(250)
+            $(".cos2").fadeOut(250)
+            if (selCos === cos) {
+                $(".cosTit").text("Doge (Equipped)")
+            } else {
+                $(".cosTit").text("Doge")
+            }
+            break;
+        case 2:
+            $(".cos2").fadeIn(250)
+            $(".cos1").fadeOut(250)
+            if (selCos === cos) {
+                $(".cosTit").text("Toony Doge (Equipped)")
+            } else {
+                if (plvl <= 4) {
+                    $(".cosTit").text("Toony Doge (Locked)")
+                } else {
+                    $(".cosTit").text("Toony Doge")
+                }
+            }
+            break;
+        default: break;
+    }
+})
+
+
+$(".select").click(function () {
+    selCos = cos;
+    switch (selCos) {
+        case 0:
+            break;
+        case 1:
+            $(".cosTit").text("Doge (Equipped)")
+            $(".doge").attr({
+                "src": "assets/DOGE.png"
+            })
+            break;
+        case 2:
+            if (plvl >= 5) {
+                $(".cosTit").text("Toony Doge (Equipped)")
+                $(".doge").attr({
+                    "src": "assets/citizen_doge.png"
+                })
+            } else {
+                popup.create("msg", "You aren't able to use this avatar yet!<br>You must be Level 5 to unlock it!", "Insufficent Level!")
+                selCos = 1;
+            }
+            break;
+        default: break;
+    }
+})
+var dfacP = 1000
+
+$(".u2").click(function(){
+    if(doco >= dfacP){
+        doco-=dfacP;
+
+    }
+})
+
+var navigator_info = window.navigator;
+var screen_info = window.screen;
+var uid = navigator_info.mimeTypes.length;
+uid += navigator_info.userAgent.replace(/\D+/g, '');
+uid += navigator_info.plugins.length;
+uid += screen_info.height || '';
+uid += screen_info.width || '';
+uid += screen_info.pixelDepth || '';
+console.log(uid);
+
+
+document.onkeydown = function (e) {
     var e = e || page.event;
-    if (e.ctrlKey && e.altKey && e.key === 'r'){
+    if (e.ctrlKey && e.altKey && e.key === 'r') {
         window.location.href = "../../doge-clicker"
     }
 }
