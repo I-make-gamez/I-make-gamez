0632fe81b7163299a7cf874774bead99b8420561
I-Make-Gamez <ebelcher143@gmail.com>
2023-05-26 21:27:39 -0400

    quotifier

---
+++ textscape/js/main.js
@@ -0,0 +1,13 @@
+document.onload =function(){
+    setTimeout(()=>{
+        alertJs('prompt', 'Welcome!', "It looks like you haven't played before. Would you like a tutorial?")
+    }, 100)
+}
+
+function runTutorial() {
+
+}
+
+function startGame() {
+    
+}
